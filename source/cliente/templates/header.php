833f0e5f5769d957616ee5294fd128a9b31a7b99
brianbruno <brian.bruno@outlook.com>
2017-09-23 21:05:58 -0300

    Atualização

---
+++ source/cliente/templates/header.php
@@ -0,0 +1,8 @@
+<nav>
+    <div class="nav-wrapper blue grey darken-4">
+        <a href="#!" class="brand-logo">Libri Factories</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="badges.html"><i class="material-icons right">account_circle</i>Login</a></li>
+        </ul>
+    </div>
+</nav>
