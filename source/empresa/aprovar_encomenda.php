c8ca7a059859e38ff4ea0c03cf8fa9631f557e6a
brianbruno <brian.bruno@outlook.com>
2017-09-23 21:42:17 -0300

    Alterações na tela de aprovar encomendas

---
+++ source/empresa/aprovar_encomenda.php
@@ -0,0 +1,29 @@
+<h1>Aprovar solicitações de compra</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>COD.</th>
+            <th>Produtos</th>
+            <th>Valor total</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>102322</td>
+            <td>Listar</td>
+            <td>R$ 32,25</td>
+        </tr>
+        <tr>
+            <td>102320</td>
+            <td>Listar</td>
+            <td>R$ 4.856,23</td>
+        </tr>
+        <tr>
+            <td>102300</td>
+            <td>Listar</td>
+            <td>R$ 7.856.74</td>
+        </tr>
+    </tbody>
+</table>
