49a2dd34785246912273d7410e3f590cdd4270f3
E2000 <lucas.silveira.2010@hotmail.com>
2017-09-28 21:52:25 -0300

    Novas telas e correção bugs
    Corrigido erro com GET vazio no index.php
    Adicionada tela de meus pedidos (cliente) e controle pedidos (empresa)
    Gambiarra do modal corrigida

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-    $opcao = $_GET['opcao'];
+    $opcao = ""; 
+    if (count($_GET) > 0) $opcao = $_GET['opcao'];
 
     switch ($opcao) {
         case "aprovarencomenda":
@@ -8,6 +9,12 @@
             break;
         case "empresainicio":
             include('source/empresa/inicio.php');
+            break;
+        case "controlepedidos":
+            include('source/empresa/Controle de Pedidos - Produção/controlepedidos.php');
+            break;
+        case "meuspedidos":
+            include('source/cliente/Meus Pedidos - Cliente/meuspedidos.php');
             break;
         default:
             include ('source/inicio.php');
