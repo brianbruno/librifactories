cf54c9327b8477599c4eb223efb24ef117f2662f
Rafael <30667950+Badarinho@users.noreply.github.com>
2017-10-06 09:51:21 -0300

    Tela de orçamento
    Mudanças no html da pagina

--- source/empresa/orcamento.php
+++ source/empresa/orcamento.php
@@ -9,11 +9,8 @@
 <style>
 h2{
     font-size: 35px;
-    margin: 50px;
+    margin: 12px;
  }
- ul{
-        font-size: 17px;
-    }
 img{
          position: absolute;
         right: 0px;
@@ -22,24 +19,28 @@ img{
     }
     h3,h4{
         font-size:20px;
-        margin: 50px;
+	margin: 12px;
     }
-    #errado { 
-    position: absolute;
-    left: 50px;
+	#errado{
+		margin: 12px;
 }
-    #confirma{
-         position: absolute;
-         right: 70px;   
+    #confirma{ 
+		position: static;
+		display: inline-block;
+		float: right;
+		margin-right: 10px;
 }
     #modifica{
-         position: absolute;
-        right: 300px;   
+		position: static;
+		display: inline-block;
+		float: right;
+		margin-right: 10px;
+          
 }
     .linha{
           border-bottom: 6px solid grey;
           margin: auto;
-          width: 93%;
+          width: 100%;
 }
 </style>
     <h1>
@@ -49,6 +50,7 @@ img{
         </h1>
     </head>
 <body>
+<div class="container">
     <!--Linha1--> 
     <div class="linha">
     </div>
@@ -59,31 +61,30 @@ img{
         </h2>
     </div>
  <!--Lista do pedido -->    
-    <div class="container">
-     <ul>
-  <li> 
-    <div class="input-field inline">
-    <input id="quant"  class="validate">
-    <label>Quantidade</label>
-     </div>
-      Nome do item
- </li>
- <li> 
-    <div class="input-field inline">
-    <input id="quant"  class="validate">
-    <label>Quantidade</label>
-     </div>
-     Nome do item
- </li>
-<li> 
-    <div class="input-field inline">
-    <input id="quant"  class="validate">
-    <label>Quantidade</label>
-     </div>
-      Nome do item
- </li>    
-     </ul>  
-         </div>
+    <!--Lista do pedido -->
+  <div class="col s12 m4 l8">
+ <table style="height: 5px; width: 574px;" class="bordered">
+ <thead>
+	<tr>
+	<th style="width: 221px;">Nome</th>
+	<th style="width: 301px;">Quantidade</th>
+	<th style="width: 400px;">Preço</th>
+	</tr>
+</thead>
+   <tbody>
+	<tr>
+	  <td style="width: 221px;">Plastico</td>
+	  <td style="width: 301px;">20</td>
+	  <td style="width: 400px;">$15,00</td>
+     </tr>
+	<tr>
+	  <td style="width: 221px;">Compartimento</td>
+	  <td style="width: 301px;">250</td>
+	  <td style="width: 400px;">$150,00</td>
+     </tr>
+   </tbody>
+ </table>
+</div>
 <!--Linha2-->    
     <div class="linha">
     </div>
@@ -91,17 +92,9 @@ img{
  <div class="row">
     <h3>
         Estimativa de preço:
-          <div class="input-field inline">
-            <input id="preco">
-              <label>Preco aqui</label>
-          </div>
      </h3>
      <h4>
          Prazo de entrega:
-          <div class="input-field inline">
-            <input id="prazo">
-              <label>Prazo aqui</label>
-          </div>
      </h4>
     </div>
  <!--Footer -->   
@@ -114,5 +107,6 @@ img{
     <button type="button"  class="waves-effect waves-light btn" id="confirma" >Confirmar pedido</button> 
  
     </div>   
+</div>
  </body>      
 </html>
