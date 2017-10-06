e3d135ba2447964af86874fb6cd4a6da0dc5decc
E2000 <lucas.silveira.2010@hotmail.com>
2017-10-06 19:28:26 -0300

    Melhoria tela de orçamento

--- source/empresa/orcamento.php
+++ source/empresa/orcamento.php
@@ -7,13 +7,18 @@
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>      
 <style>
+body {
+    background-image: url("https://files.slack.com/files-tmb/T6WGP2WAU-F77TSB5QD-9191f5efb3/fundo_720.jpg");  
+}
 h2{
     font-size: 35px;
     margin: 12px;
  }
 img{
-         position: absolute;
-        right: 0px;
+    position: static;
+    display: inline-block;
+    float: right;
+    margin-right: 0;
         width:150px;
         height:140px;
     }
@@ -24,45 +29,62 @@ img{
 	#errado{
 		margin: 12px;
 }
-    #confirma{ 
-		position: static;
-		display: inline-block;
-		float: right;
-		margin-right: 10px;
-}
     #modifica{
 		position: static;
 		display: inline-block;
 		float: right;
 		margin-right: 10px;
+		margin-top: 12px;
           
+}
+    #confirma{ 
+		position: static;
+		display: inline-block;
+		float: right;
+		margin-right: 10px;
+		margin-top: 12px;
 }
     .linha{
           border-bottom: 6px solid grey;
           margin: auto;
           width: 100%;
 }
+    table {
+        display: inline-block;
+    }
+    
+    .tabela {
+    }
+    
+.container {
+    position: relative;
+	margin-top: 20px;
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+    margin-bottom: 220px;
+    overflow: hidden;
+}
 </style>
+    </head>
+<body>
+<div class="container">
     <h1>
         <center>
         Valor do orçamento
         </center>
         </h1>
-    </head>
-<body>
-<div class="container">
     <!--Linha1--> 
-    <div class="linha">
-    </div>
+    <div class="linha"></div>
     <div class="row">
     <h2>
         Itens do pedido:
-        <img src="imagens/librifactories.jpg">
         </h2>
     </div>
  <!--Lista do pedido -->    
     <!--Lista do pedido -->
-  <div class="col s12 m4 l8">
+  <div class="col s12 m4 l8 tabela">
+        <img src="source/empresa/libri.jpg">
  <table style="height: 5px; width: 574px;" class="bordered">
  <thead>
 	<tr>
@@ -99,12 +121,13 @@ img{
     </div>
  <!--Footer -->   
     
+    <div class="linha"></div>
     <div class="row">
         
     <button type="button" class="waves-effect waves-light btn" id="errado">Algo de errado?</button>
-        
-    <button type="button" class="waves-effect waves-light btn" id="modifica" c>Modificar pedido</button>    
+         
     <button type="button"  class="waves-effect waves-light btn" id="confirma" >Confirmar pedido</button> 
+    <button type="button" class="waves-effect waves-light btn" id="modifica">Modificar pedido</button>   
  
     </div>   
 </div>
